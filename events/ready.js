76c9550b3381f7077f029559903a66037b8b0cbd
Androz2091 <androz2091@gmail.com>
2022-07-12 08:26:47 +0200

    clean up

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,4 @@
-const globals = require('../util/globals');
-const puppeteer = require('puppeteer');
 const { apiCall } = require('../kartra/api-call');
-const login = require('../trading-view/login');
-const addUser = require('../trading-view/addUser');
-const removeUser = require('../trading-view/removeUser');
 
 module.exports = async (bot) => {
 	console.log(`${bot.user.username} is online!`);
