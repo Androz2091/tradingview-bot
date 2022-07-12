6d3ab9052c2853189a4b0ed2087c34fc21b93aea
Androz2091 <androz2091@gmail.com>
2022-07-12 09:33:23 +0200

    remove server

--- events/ready.js
+++ events/ready.js
@@ -2,8 +2,4 @@ const { apiCall } = require('../kartra/api-call');
 
 module.exports = async (bot) => {
 	console.log(`${bot.user.username} is online!`);
-
-	const server = require('../util/server.js');
-
-	server.emitter.on('hook', apiCall);
 };
